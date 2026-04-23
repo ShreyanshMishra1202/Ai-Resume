0ecf5dbd6cc2a3ed736dff7aa6875f65ce9c7455
Shreyansh Mishra <shreyanshmishra1202@gmail.com>
2026-04-24 00:19:13 +0530

    Shreyansh Mishra: protect builder route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import Home from './pages/Home.jsx'
 import Builder from './pages/Builder.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="builder" element={<Builder />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="builder" element={<Builder />} />
+        </Route>
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
